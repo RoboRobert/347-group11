4dc8b57ba76257996997283a94ae82f88b1b3a2b
Nathan Estep <nathan.estep@gmail.com>
2024-11-24 00:12:54 -0600

    playing with URP

--- Assets/Scripts/Levels/LevelGenerator.cs
+++ Assets/Scripts/Levels/LevelGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -8,6 +9,8 @@ public class LevelGenerator : MonoBehaviour
     public int roomSize = 18;
     public Vector2 startPos = new Vector2(2, 2);
 
+    public GameObject room;
+
     private enum DirEnum {
         UP,DOWN,LEFT,RIGHT
     }
@@ -15,15 +18,31 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (room == null)
+            return;
         //The Level Generator runs when the scene is first initialized
         int[,] rooms = new int[5,5];
 
-        // Generate rooms starting at the start pos until the number of rooms is 
+        Vector2 currentRoom = startPos;
+
+        // Generate rooms starting at the start pos until the number of rooms is satisfied
+        for(int i = 0; i < roomSize; i++)
+        {
+            DirEnum direction = RandDirection();
+            //while ()
+        }
     }
     
     // Gets a random direction to move in
-    static DirEnum RandDirection() {
+    DirEnum RandDirection() {
         DirEnum[] dirArray = {DirEnum.UP, DirEnum.DOWN,DirEnum.LEFT,DirEnum.RIGHT};
         return (DirEnum)LootTable.PickRandom(dirArray);
     }
+
+    bool ValidIndex(Vector2 index, Vector2 bounds) {
+        if (index.x > bounds.x || index.y > bounds.y) return false;
+        return true;
+    }
+
+    //Vector2 Move()
 }
