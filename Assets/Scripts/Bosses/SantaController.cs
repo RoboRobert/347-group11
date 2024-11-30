b26c10760c3d7f4f7d19e3cc4c1f7747624801fb
Nathan Estep <nathan.estep@gmail.com>
2024-11-30 01:42:23 -0600

    santa pretty much done

--- Assets/Scripts/Bosses/SantaController.cs
+++ Assets/Scripts/Bosses/SantaController.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class SantaController : MonoBehaviour
 {
     [Header("Set Dynamically")]
-    
-    public float health = 100f;
     public float speed = 3f;
 
     //Follow distance in grid squares of 32 pixels each
     public float followDistance = 2f;
 
     private Rigidbody2D _rb;
-    public GameObject _target;
+    private GameObject _target;
 
     // Start is called before the first frame update
     void Start()
@@ -22,38 +20,12 @@ public class SantaController : MonoBehaviour
         _target = GameObject.FindWithTag("Player");
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     
-    // }
-
     void FixedUpdate()
     {
+        if (GetComponentInParent<StatManager>().dead)
+        {
+            return;
+        }
         //Calls the movement logic for the enemy
-        movement_func();
-
-        if (health <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    // Enemy movement logic
-    void movement_func()
-    {
-        //Amount to move the current enemy toward the target
-        Vector3 movementVector = Vector3.zero;
-        // Gets a normalized distance to the target.
-        Vector3 targetDir = _target.transform.position - _rb.transform.position;
-
-        //If the distance to the target is large enough, move toward the target
-        if (targetDir.magnitude > followDistance * 0.32)
-        {
-            movementVector = targetDir.normalized * (speed * Time.fixedDeltaTime);
-        }
-
-        //Apply the movement vector
-        _rb.velocity = movementVector;
     }
 }
--- Assets/Scripts/Bosses/SantaStateMachine.cs
+++ Assets/Scripts/Bosses/SantaStateMachine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SantaStateMachine : MonoBehaviour
 {
+    public AnimationClip attackUp;
+    public AnimationClip attackDown;
     public AnimationClip sideRun;
     public AnimationClip sideIdle;
     public AnimationClip frontRun;
@@ -12,16 +14,27 @@ public class SantaStateMachine : MonoBehaviour
     public AnimationClip backIdle;
     public AnimationClip death;
 
+    public GameObject attack;
+
+    public float speed = 3f;
+
+    //Follow distance in grid squares of 32 pixels each
+    public float followDistance = 2f;
+
     private Animator _animator;
     private AnimationClip currentAnimation;
-    private AnimationClip currentIdle;
-    private Rigidbody2D _parentBody;
+    private Rigidbody2D _rb;
     private GameObject _target;
+
+    private string _state = "IDLE";
+    private string _substate = "FRONT";
+
+    private float attackOffset = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _parentBody = GetComponentInParent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         _target = GameObject.FindWithTag("Player");
 
         //Sets the default animation to be front idle
@@ -41,60 +54,105 @@ public class SantaStateMachine : MonoBehaviour
     void Update()
     {
         ChangeState();
+
+        if (_state == "DEAD")
+        {
+            _rb.velocity = Vector2.zero;
+            ChangeAnimation(death);
+            return;
+        }
+        if(_state == "IDLE")
+        {
+            _rb.velocity = Vector2.zero;
+            if (_substate == "UP")
+            {
+                ChangeAnimation(backIdle);
+            }
+            if (_substate == "DOWN")
+            {
+                ChangeAnimation(frontIdle);
+            }
+            if (_substate == "SIDE")
+            {
+                ChangeAnimation(sideIdle);
+            }
+        }
+        if (_state == "RUN")
+        {
+            movement_func();
+            if (_substate == "UP")
+            {
+                ChangeAnimation(backRun);
+            }
+            if (_substate == "DOWN")
+            {
+                ChangeAnimation(frontRun);
+            }
+            if (_substate == "SIDE")
+            {
+                ChangeAnimation(sideRun);
+            }
+        }
+        if (_state == "ATTACK")
+        {
+            _rb.velocity = Vector2.zero;
+            if (_substate == "UP")
+            {
+                attackOffset = 0.3f;
+                ChangeAnimation(attackUp);
+            }
+            if (_substate == "DOWN")
+            {
+                attackOffset = -0.3f;
+                ChangeAnimation(attackDown);
+            }
+            if (_substate == "SIDE")
+            {
+                attackOffset = -0.3f;
+                ChangeAnimation(attackDown);
+            }
+        }
     }
 
     void ChangeState()
     {
+        if (GetComponentInParent<StatManager>().dead)
+            _state = "DEAD";
+
+        if (_state == "ATTACK" || _state == "DEAD")
+            return;
         Vector3 currentScale = transform.localScale;
 
-        Vector3 difference = _target.transform.position - transform.position;
+        Vector3 difference = _target.transform.position - _rb.transform.position;
 
-        Vector3 vel = _parentBody.velocity;
+        Vector3 vel = _rb.velocity;
 
-        //If the parent body isn't moving, play the current idle animation.
-        if (vel.magnitude == 0)
+        //If the player is close enough, attack them.
+        if (difference.magnitude < followDistance * 0.32)
         {
-            ChangeAnimation(currentIdle);
+            _state = "ATTACK";
+            Invoke("FinishAttack", 2.5f);
+            return;
         }
+        // Otherwise 
+        else _state = "RUN";
 
-        //If the parent body is moving horizontally more than vertically
-        if (Mathf.Abs(vel.x) > Mathf.Abs(vel.y))
-        {
-            ChangeAnimation(sideRun);
-        }
-        //If the parent body is moving vertically more than horizontally
-        else
-        {
-            // Handle moving down
-            if (vel.y < 0)
-            {
-                ChangeAnimation(frontRun);
-            }
-            //Handle moving up
-            if (vel.y > 0)
-            {
-                ChangeAnimation(backRun);
-            }
-        }
-
-        // If the X difference is larger, use side idle
+        // If the X difference is larger, use SIDE substates
         if (Mathf.Abs(difference.x) > Mathf.Abs(difference.y))
         {
-            currentIdle = sideIdle;
+            _substate = "SIDE";
         }
 
-        // If the Y difference is larger, use front or back idle
+        // If the Y difference is larger, use DOWN or UP substates
         else
         {
-            // Front Idle
             if (difference.y < 0)
             {
-                currentIdle = frontIdle;
+                _substate = "DOWN";
             }
-            // Back Idle
             if (difference.y > 0)
             {
-                currentIdle = backIdle;
+                _substate = "UP";
             }
         }
 
@@ -106,5 +164,36 @@ public class SantaStateMachine : MonoBehaviour
         else if (difference.x < 0) currentScale.x = 1;
 
         transform.localScale = currentScale;
+    }
+
+    // Enemy movement logic
+    void movement_func()
+    {
+        //Amount to move the current enemy toward the target
+        Vector3 movementVector = Vector3.zero;
+        // Gets a distance to the target.
+        Vector3 targetDir = _target.transform.position - _rb.transform.position;
+
+        //If the distance to the target is large enough, move toward the target
+        if (targetDir.magnitude > 0)
+        {
+            movementVector = targetDir.normalized * (speed * Time.fixedDeltaTime);
+        }
+
+        //Apply the movement vector
+        _rb.velocity = movementVector;
+    }
+
+    void CreateAttack()
+    {
+        GameObject newAttack = Instantiate(attack, transform);
+        newAttack.transform.localPosition = new Vector2(0, attackOffset);
+    }
+
+    //Breaks out of the attack state
+    void FinishAttack()
+    {
+        _state = "IDLE";
+        ChangeAnimation(frontIdle);
     }
 }
