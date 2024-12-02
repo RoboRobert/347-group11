40119f41772598e5e12c11be7eda2eaca924c003
mxstff <maxstofer1@gmail.com>
2024-12-01 23:19:51 -0600

    add the muscular dog

---
+++ Assets/dog.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public GameObject target;
+    public float speed = 20f;
+
+    //Follow distance in grid squares of 32 pixels each
+    public float followDistance = 2f;
+
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        
+        //Calls the movement logic for the enemy
+        movement_func();
+    }
+
+    // Enemy movement logic
+    void movement_func()
+    {
+        //Amount to move the current enemy toward the target
+        Vector3 movementVector = Vector3.zero;
+        // Gets a normalized distance to the target.
+        Vector3 targetDir = target.transform.position - _rb.transform.position;
+
+        //If the distance to the target is large enough, move toward the target
+        if (targetDir.magnitude > followDistance * 0.32)
+        {
+            movementVector = targetDir.normalized * (speed * Time.fixedDeltaTime);
+        }
+
+        // if the distance is too crazy, teleport
+        if (targetDir.magnitude > followDistance )
+        {
+            Vector3 newposition = targetDir;
+            newposition.z -= .2f;
+            _rb.transform.position = newposition;
+        }
+
+        //Apply the movement vector
+        _rb.velocity = movementVector;
+    }
+}
